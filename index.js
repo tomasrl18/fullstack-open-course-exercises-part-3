5c74fa32eb551e48bf4dd9c864bc181cdcb131e4
Tomás R <tomaslopezraigal@gmail.com>
2024-09-21 13:45:34 +0200

    Done exercise 3.5: phonebook step5

--- index.js
+++ index.js
@@ -65,6 +65,32 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    return maxId = persons.length > 0
+            ? Math.max(...persons.map(n => n.id)) 
+            : 0
+}
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
