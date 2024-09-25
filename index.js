93e8471a9187cc9084cc520aa4290235d9b665e0
Tomás R <tomaslopezraigal@gmail.com>
2024-09-25 19:55:41 +0200

    Done exercises 3.16-3.17: phonebook steps4-5

--- index.js
+++ index.js
@@ -44,10 +44,15 @@ app.get('/api/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -77,6 +82,21 @@ app.post('/api/persons/', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))    
 })
 
 const unknownEndpoint = (request, response) => {
