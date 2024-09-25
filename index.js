81ed2abba661e0caff2191cd9e23f2e4adb24f41
Tomás R <tomaslopezraigal@gmail.com>
2024-09-25 19:28:05 +0200

    Done exercises 3.14-3.15: phonebook steps2-3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
@@ -12,6 +12,16 @@ app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
 
 const getDate = () => {
     return new Date()
@@ -40,12 +50,12 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    let personToDelete = Person.findById(request.params.id)
-
-    Person.deleteOne(personToDelete).then(person => {
-        console.log('Deleted successfully');
-    })
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response) => {
@@ -68,6 +78,24 @@ app.post('/api/persons/', (request, response) => {
       response.json(savedPerson)
     })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
